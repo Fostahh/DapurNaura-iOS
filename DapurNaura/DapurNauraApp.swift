5ae41ddd9be6305117acc25251e4fed75c3f8203
Mohammad Azri Khairuddin <mohammad.azri@visionet.co.id>
2026-06-10 10:00:55 +0700

    Initial Commit

---
+++ DapurNaura/DapurNauraApp.swift
@@ -0,0 +1,17 @@
+//
+//  DapurNauraApp.swift
+//  DapurNaura
+//
+//  Created by Mohammad Azri Khairuddin on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct DapurNauraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DapurNauraTests/DapurNauraTests.swift
@@ -0,0 +1,17 @@
+//
+//  DapurNauraTests.swift
+//  DapurNauraTests
+//
+//  Created by Mohammad Azri Khairuddin on 10/06/26.
+//
+
+import Testing
+@testable import DapurNaura
+
+struct DapurNauraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
